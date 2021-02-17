618b5833895cdf5d5b49efa3c1173074b6a01744
HeljaH <>
2021-02-17 13:29:28 +0200

    ASPNET harjoituksia

---
+++ DotNet/server/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using server.Models;
+
+namespace server.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult myProfile()
+        {
+            return View();
+        }
+        
+        public IActionResult GameHouse()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GameHouse(int luku1, int luku2)
+        {
+            int summa = luku1 + luku2;
+            ViewBag.tulos = summa;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DotNet/server/wwwroot/js/sovellus.js
@@ -0,0 +1,9 @@
+// jQuery esimerkki. :O
+$("#nappi").click(function () {
+    alert('Moro moro JavaScriptilla!')
+});
+
+$("#calculate").click(function () {
+    const summa = $("#first") + $("#second");
+    console.log(summa);
+});
